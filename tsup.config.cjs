28c3b49c32a23e4d8d3063a3f3e95ee4f8630357
skullxcode <pranavthawait@gmail.com>
2026-07-26 17:34:08 +0530

    l

--- tsup.config.cjs
+++ tsup.config.cjs
@@ -1,6 +1,4 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
+module.exports = {
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
@@ -10,7 +8,7 @@ export default defineConfig({
   treeshake: true,
   external: ['react', 'react-dom'],
   banner: {
-    js: '/* @driveloader/react v1.1.0 | MIT License | https://github.com/Pranav00076/driveLoader */',
+    js: '/* @driveloader/react v1.2.0 | MIT License | https://github.com/Pranav00076/driveLoader */',
   },
   onSuccess: 'cp src/styles.css dist/styles.css 2>/dev/null || true',
-});
+};
